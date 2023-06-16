0a81d15d1ced05e83c4ba38051f4bf0accfbb247
slee <slee@magicsoft.co.kr>
2023-06-16 19:08:01 +0900

    JPA를 사용하는 코드

--- src/main/java/hello/itemservice/domain/Item.java
+++ src/main/java/hello/itemservice/domain/Item.java
@@ -2,17 +2,23 @@ package hello.itemservice.domain;
 
 import lombok.Data;
 
+import javax.persistence.*;
+
 @Data
+@Entity
 public class Item {
 
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    // @Column 생략 가능 자동으로 카멜 케이스를 언더스코어로 자동으로 변환해준다.
+    @Column(name = "item_name", length = 100)
     private String itemName;
     private Integer price;
     private Integer quantity;
 
-    public Item() {
-    }
+    // JPA는 기본 생성자는 필수
+    public Item() {}
 
     public Item(String itemName, Integer price, Integer quantity) {
         this.itemName = itemName;
